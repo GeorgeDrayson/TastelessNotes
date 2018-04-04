f93d2d27bc99cd2d34740c617463840498915f83
George Drayson <drayson.g@mac.com>
2018-04-04 15:53:29 +0100

    Added fully tested note and notebook class (all logic)

---
+++ src/notebook.js
@@ -0,0 +1,11 @@
+function Notebook() {
+  this._notes = []
+}
+
+Notebook.prototype.add = function(note) {
+  this._notes.push(note)
+}
+
+Notebook.prototype.all = function() {
+  return this._notes
+}
