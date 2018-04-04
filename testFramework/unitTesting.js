65488f50a4c92bcd96c60510903e8f072444863e
Christopher Ly <demedial@gmail.com>
2018-04-04 11:59:26 +0100

    Remove true statements from test

--- testFramework/unitTesting.js
+++ testFramework/unitTesting.js
@@ -3,39 +3,39 @@
   function expect(object) {
     return {
       toEqual: function(check) {
-        return object === check ?
-        `PASS: ${object} is equal to ${check}` :
-        `FAIL: ${object} is not equal to ${check}`;
+        if (object !== check) {
+        return `FAIL: ${object} is not equal to ${check}`;
+        }
       },
 
       toNotEqual: function(check) {
-        return object !== check ?
-        `PASS: ${object} is not equal to ${check}` :
-        `FAIL: ${object} is NOT not equal to ${check}...?!?!`;
+        if (object === check) { 
+        return `FAIL: ${object} is NOT not equal to ${check}...?!?!`;
+        }
       },
 
       toBeTrue: function() {
-        return object === true ?
-        `PASS: ${object} is true` :
-        `FAIL: ${object} is not true`;
+        if (object !== true) {
+        return `FAIL: ${object} is not true`;
+        }
       },
 
       toNotBeTrue: function() {
-        return object !== true ?
-        `PASS: ${object} is not true` :
-        `FAIL: ${object} is NOT not true...?!?!`;
+        if (object === true) {
+        return `FAIL: ${object} is NOT not true...?!?!`;
+        }
       },
 
       toContain: function(content) {
-        return object.includes(content) ?
-        `PASS: ${object} contains ${content}` :
-        `FAIL: ${object} does not contain ${content}`;
+        if (!object.includes(content)) {
+        return `FAIL: ${object} does not contain ${content}`;
+        }
       },
 
       toNotContain: function(content) {
-        return !object.includes(content) ?
-        `PASS: ${object} does not contain ${content}` :
-        `FAIL: ${object} does NOT not contain ${content}...WTF?`;
+        if (object.includes(content)) {
+        return `FAIL: ${object} does NOT not contain ${content}...WTF?`;
+        }
       }
     }
   }
