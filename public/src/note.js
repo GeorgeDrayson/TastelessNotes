1b94d9252060b1ca87e5a77bd9a122e4dcd45f5a
julesnuggy <julesnuggy@gmail.com>
2018-04-05 11:18:34 +0100

    Added fully tested methods to notebook for adding HTML elements@

--- public/src/note.js
+++ public/src/note.js
@@ -6,6 +6,6 @@ Note.prototype.getBody = function() {
   return this._body
 }
 
-Note.prototype.abbrBody = function (n) {
+Note.prototype.abbrBody = function() {
   return (this._body.length > 20 ? this._body.slice(0, 20) + '...' : this._body);
 }
--- public/src/notebook.js
+++ public/src/notebook.js
@@ -9,3 +9,16 @@ Notebook.prototype.add = function(note) {
 Notebook.prototype.all = function() {
   return this._notes
 }
+
+Notebook.prototype.addToList = function() {
+  var newListElement = document.createElement("li");
+  var newContent = document.createTextNode(
+    this._notes[this._notes.length-1].abbrBody())
+  newListElement.appendChild(newContent);
+  document.getElementById("notes_list").appendChild(newListElement)
+}
+
+Notebook.prototype.addNote = function(note) {
+  this.add(note)
+  this.addToList()
+}
